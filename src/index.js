cb9c4181d374524792d38a846ad9c3e9b8b19787
Andreas Lee-Norman <andreas.lee-norman@knowit.se>
2021-01-14 16:23:40 +0100

    no message

--- src/index.js
+++ src/index.js
@@ -17,14 +17,13 @@ var allowedOrigins = ['http://localhost:3000', 'http://localhost:4000']
 app.use(
 	cors({
 		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true)
 			if (allowedOrigins.indexOf(origin) === -1) {
-				var msg =
-					'The CORS policy for this site does not ' +
-					'allow access from the specified Origin.'
-				return callback(new Error(msg), false)
+				return callback(
+					new Error(
+						'The CORS policy for this site does not allow access from the specified Origin.'
+					),
+					false
+				)
 			}
 			return callback(null, true)
 		},
